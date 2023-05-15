e6b68f57266a4e2f20013eaf66bba14ae9c4c8c1
Cory Marshall <corrmaan@gmail.com>
2023-05-15 11:06:41 -0300

    Updates

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-sudo dnf config-manager --set-enabled crb
-sudo dnf install epel-release
-sudo dnf update
-sudo dnf install gcc rpm-build rpm-devel rpmlint make python3 bash coreutils diffutils patch rpmdevtools
+sudo dnf -y config-manager --set-enabled crb
+sudo dnf -y install epel-release
+sudo dnf -y update
+sudo dnf -y install gcc rpm-build rpm-devel rpmlint make python3 bash coreutils diffutils patch rpmdevtools
 
-dnf download --disablerepo=* \
+dnf -y download --disablerepo=* \
     --repofrompath="fc36,https://download.fedoraproject.org/pub/fedora/linux/releases/36/Everything/source/tree/" \
     --source spice-gtk spice spice-protocol libcacard
-dnf download --source virt-manager qemu-kvm
+dnf -y download --disablerepo=* --enablerepo=appstream --source virt-manager qemu-kvm
 
 rpmdev-setuptree
 rpm -ivh *.rpm
@@ -37,30 +37,30 @@ git add spice-gtk.spec && git commit -m "spice-gtk.spec patch"
 patch -p1 < ${OLDPWD}/virt-manager.spec.patch
 git add virt-manager.spec && git commit -m "virt-manager.spec patch"
 
-sudo dnf builddep libcacard.spec
+sudo dnf -y builddep libcacard.spec
 rpmbuild -ba libcacard.spec
 
-sudo dnf builddep  spice-protocol.spec
+sudo dnf -y builddep spice-protocol.spec
 rpmbuild -ba spice-protocol.spec
 
-sudo dnf install \
-    ~/rpmbuild/RPMS/x86_64/libcacard-2.8.1-2.el9.x86_64.rpm \
-    ~/rpmbuild/RPMS/x86_64/libcacard-devel-2.8.1-2.el9.x86_64.rpm
+sudo dnf -y install \
+    ~/rpmbuild/RPMS/x86_64/libcacard-2.*.rpm \
+    ~/rpmbuild/RPMS/x86_64/libcacard-devel-2.*.rpm
 
-sudo dnf builddep spice.spec
+sudo dnf -y builddep spice.spec
 rpmbuild -ba spice.spec
 
-sudo dnf install \
-    ~/rpmbuild/RPMS/x86_64/spice-server-0.15.0-4.el9.x86_64.rpm \
-    ~/rpmbuild/RPMS/x86_64/spice-server-devel-0.15.0-4.el9.x86_64.rpm
+sudo dnf -y install \
+    ~/rpmbuild/RPMS/x86_64/spice-server-0.*.rpm \
+    ~/rpmbuild/RPMS/x86_64/spice-server-devel-0.*.rpm
 
-sudo dnf builddep qemu-kvm.spec
+sudo dnf -y builddep qemu-kvm.spec
 rpmbuild -ba qemu-kvm.spec
 
-sudo dnf builddep spice-gtk.spec
+sudo dnf -y builddep spice-gtk.spec
 rpmbuild -ba spice-gtk.spec
 
-sudo dnf builddep virt-manager.spec
+sudo dnf -y builddep virt-manager.spec
 rpmbuild -ba virt-manager.spec
 
 cd -
